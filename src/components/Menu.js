969ddcd5b7797df33bcffb5d6470a02b908d5b84
Melanie De Giusti <meludegiusti@Melanies-MacBook-Pro.local>
2021-11-12 20:48:02 -0300

    login y register

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -5,23 +5,63 @@ import {createDrawerNavigator} from '@react-navigation/drawer';
 import Home from '../screens/Home';
 import Login from '../screens/Login';
 import Register from '../screens/Register';
+import {auth} from '../firebase/config'
 
 const Drawer = createDrawerNavigator()
 
 class Menu extends Component {
     constructor(){
         super();
-        this.setState ={}
+        this.state ={
+            loggedIn: false,
+            userData:'',
+        }
+    }
+
+    componentDidMount() {
+        auth.onAuthStateChanged((user) => {
+            console.log(user);
+            if (user !== null) {
+                this.setState({
+                    loggedIn: true,
+                })
+            } else {
+                this.setState({
+                    loggedIn: false,
+                })
+            }
+        })
+    }
+
+    register(email, password){
+        auth.createUserWithEmailAndPassword(email, password)
+            .then((userData)=> console.log(userData))
+            .catch((err)=> console.log(err))
+    }
+
+    login(email, password){
+         auth.signInWithEmailAndPassword(email, password)
+             .then((response)=> {
+                console.log("se logueó correctamente");
+                console.log(response);
+                this.setState({
+                    loggedIn: true,
+                    userData: response.user
+                })
+             
+            })
+             .catch((err)=> console.log(err))
     }
 
     render(){
         return(
-           
                 <Drawer.Navigator>
                     <Drawer.Screen name="Home" component={()=> <Home />} />
-                    <Drawer.Screen name="Login" component={()=> <Login />}/>
-                    <Drawer.Screen name="Register" component={()=> <Register />}/>
+                    <Drawer.Screen name="Login" component={()=> <Login login={(email, pass)=>this.login(email, pass)}/> }/>
+                    <Drawer.Screen name="Register" component={()=> <Register register={(email, pass)=> this.register(email, pass) }/> }/>
+                    <Drawer.Screen name="Mi Perfil" component={()=><Profile logout={() => this.logout()} />} />
                 </Drawer.Navigator>
+            
             
         )
     }
--- src/screens/Register.js
+++ src/screens/Register.js
@@ -1,17 +1,84 @@
 import React, {Component} from 'react'
-import{Text, View} from 'react-native'
+import {
+    Text, 
+    View, 
+    TextInput, 
+    TouchableOpacity, 
+    StyleSheet
+} from 'react-native'
+import {auth} from '../firebase/config'
+
 
 class Register extends Component{
     constructor(props){
         super(props);
-        this.setState ={}
+        this.state = {
+            email:'',
+            userName:'',
+            password:'',
+        }
     }
 
     render(){
         return(
-            <Text>Hola</Text>
+            <View style={styles.formContainer}>
+                <Text>Register</Text>
+                <TextInput
+                    style={styles.input}
+                    onChangeText={(text)=>this.setState({email: text})}
+                    placeholder='email'
+                    keyboardType='email-address'
+                    />
+                <TextInput
+                    style={styles.input}
+                    onChangeText={(text)=>this.setState({password: text})}
+                    placeholder='password'
+                    keyboardType='default'
+                    secureTextEntry={true}
+                />
+                <Text style={styles.error}>{this.props.errorMessage}</Text>
+                <TouchableOpacity style={styles.button} onPress={()=>this.props.register(this.state.email, this.state.password)}>
+                    <Text style={styles.textButton}>Registrarse</Text>    
+                </TouchableOpacity>
+            </View>
         )
     }
 }
+const styles= StyleSheet.create({
+    formContainer:{
+        paddingHorizontal:10,
+        marginTop: 20,
+    },
+    input:{
+        height:20,
+        paddingVertical:15,
+        paddingHorizontal: 10,
+        borderWidth:1,
+        borderColor: '#ccc',
+        borderStyle: 'solid',
+        borderRadius: 6,
+        marginVertical:10,
+    },
+    error:{
+        marginBottom: 10,
+        color: "#dc3545",
+        fontSize: 12
+    },
+    button:{
+        backgroundColor:'#28a745',
+        paddingHorizontal: 10,
+        paddingVertical: 6,
+        textAlign: 'center',
+        borderRadius:4, 
+        borderWidth:1,
+        borderStyle: 'solid',
+        borderColor: '#28a745'
+    },
+    textButton:{
+        color: '#fff'
+    }
+
+})
+
 
 export default Register;
