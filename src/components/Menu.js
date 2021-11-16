f278b7ff0437424f6193074e1acd415819ff4d3c
Melanie De Giusti <meludegiusti@Melanies-MacBook-Pro.local>
2021-11-15 23:34:50 -0300

    newPosts

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -5,6 +5,8 @@ import {createDrawerNavigator} from '@react-navigation/drawer';
 import Home from '../screens/Home';
 import Login from '../screens/Login';
 import Register from '../screens/Register';
+import Profile from '../screens/Profile';
+import NewPostForm from '../screens/NewPostForm';
 import {auth} from '../firebase/config'
 
 const Drawer = createDrawerNavigator()
@@ -14,7 +16,8 @@ class Menu extends Component {
         super();
         this.state ={
             loggedIn: false,
-            userData:'',
+            errorMessage:'',
+            errorCode:'',
         }
     }
 
@@ -35,34 +38,54 @@ class Menu extends Component {
 
     register(email, password){
         auth.createUserWithEmailAndPassword(email, password)
-            .then((userData)=> console.log(userData))
-            .catch((err)=> console.log(err))
-    }
+        .then( () => {
+            console.log('========= Registrado ============');
+        })
+        .catch( error => {
+            console.log(error);
+            this.setState({
+                errorMessage: error.message,
+                errorCode: error.code
+            })
+        })
+}
 
     login(email, password){
          auth.signInWithEmailAndPassword(email, password)
-             .then((response)=> {
-                console.log("se logueó correctamente");
-                console.log(response);
-                this.setState({
-                    loggedIn: true,
-                    userData: response.user
-                })
-             
+         .then( (response)=>{
+            console.log('========= Logueado ============');
+            console.log(response);
+            this.setState({
+                loggedIn: true,
+                userData: response.user,
             })
-             .catch((err)=> console.log(err))
+        })
+        .catch( error => {
+            console.log(error);
+            this.setState({
+                errorMessage:error.message,
+                errorCode: error.code
+            })
+        })
+}    
+
+      logout(){
+          auth.signOut()
     }
 
     render(){
         return(
-                <Drawer.Navigator>
-                    <Drawer.Screen name="Home" component={()=> <Home />} />
-                    <Drawer.Screen name="Login" component={()=> <Login login={(email, pass)=>this.login(email, pass)}/> }/>
-                    <Drawer.Screen name="Register" component={()=> <Register register={(email, pass)=> this.register(email, pass) }/> }/>
-                    <Drawer.Screen name="Mi Perfil" component={()=><Profile logout={() => this.logout()} />} />
-                </Drawer.Navigator>
-            
-            
+                this.state.loggedIn == false ? 
+                    <Drawer.Navigator>
+                        <Drawer.Screen name="Login" component={()=><Login login={(email, pass)=>this.login(email, pass)} errorMessage={this.state.errorMessage} errorCode={this.state.errorCode} />} />
+                        <Drawer.Screen name="Register" component={()=><Register register={(email, pass)=>this.register(email, pass)} errorMessage={this.state.errorMessage} errorCode={this.state.errorCode} />} />
+                    </Drawer.Navigator>
+                 :
+                    <Drawer.Navigator>
+                        <Drawer.Screen name="Home" component={()=><Home />} />  
+                        <Drawer.Screen name="Nuevo Post" component={() => <NewPostForm />}  />                    
+                        <Drawer.Screen name="Mi Perfil" component={()=><Profile logout={() => this.logout()} />} />
+                    </Drawer.Navigator>
         )
     }
 }
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,17 +1,71 @@
 import React, {Component} from 'react'
-import{Text, View} from 'react-native'
+import{Text, View, StyleSheet, FlatList} from 'react-native'
+import { db } from '../firebase/config'
 
 class Home extends Component{
     constructor(props){
         super(props);
-        this.setState ={}
+        this.State ={
+            posts:[]
+        }
+    }
+
+    componentDidMount(){
+        this.showPost();
+    }
+
+    showPost(){
+        db.collection('posteos').onSnapshot((docs)=>{
+            console.log(docs)
+            let posteos = []
+            docs.forEach((doc)=> {
+                console.log(doc.data())
+                posteos.push({
+                    id: doc.id,
+                    data: doc.data()
+                })
+            })
+            this.setState({
+                posts: posteos
+            })
+        })
     }
 
     render(){
         return(
-            <Text>Hola</Text>
+            <View style={StyleSheet.container}>
+                <FlatList 
+                    // data = {this.state.posts}
+                    // keyExtractor = {(post) => post.id}
+                    // renderItem = {({item}) => <Post postData={item} />}
+                />
+            </View>
+            
+            
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        textAlign: 'center',
+        padding: 10,
+    },
+    image:{
+        height:250,
+    },
+    flatList:{
+        justifyContent:'space-between'
+    },
+    touchable:{
+        padding: 4,
+        backgroundColor: '#ccc',
+        marginBottom: 10,
+        borderRadius: 4,
+    },
+    touchableText:{
+        fontWeight: 'bold'
+    }
+});
 
 export default Home;
